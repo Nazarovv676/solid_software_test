c75e6eface3384e2658650df75b32c5b9d676056
Ivan Nazarov <nazarovv676@gmail.com>
2021-10-19 20:33:21 +0300

    Set empty ui system mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:solid_software_test/pages/main/main_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   runApp(const MyApp());
 }
 
